25fcb0456508fba77e5ded79c38d39c5ad69bfa3
pakorndiode <pakorn_sillapaprapha@cmu.ac.th>
2021-06-15 16:49:46 +0700

    tanjiroEnd

--- script2.js
+++ script2.js
@@ -52,6 +52,11 @@ var student ={};
 student.name='คุณลุง'
 student.username='a@b.com'
 student.gender='ชาย'
+var student2 ={};
+student2.name='ควาย'
+student2.username='gg@G.com'
+student2.gender='มั่ว'
+
 function addStudentData(student){
     const output = document.getElementById("output")
     let row = document.createElement("div")
@@ -67,7 +72,78 @@ function addStudentData(student){
     row.appendChild(columnName)
     row.appendChild(columnValue)
     output.appendChild(row)
+
+    row = document.createElement("div")
+    row.classList.add("row")
+    columnName = document.createElement("div")
+    columnName.classList.add("col-1")
+    columnName.classList.add("offset-1")
+    columnName.innerHTML = "Id"
+    columnValue = document.createElement("div")
+    columnValue = document.createElement("row")
+    columnValue.classList.add("col")
+    columnValue.innerHTML = student.username
+    row.appendChild(columnName)
+    row.appendChild(columnValue)
+    output.appendChild(row)
+
+    row = document.createElement("div")
+    row.classList.add("row")
+    columnName = document.createElement("div")
+    columnName.classList.add("col-1")
+    columnName.classList.add("offset-1")
+    columnName.innerHTML = "gender"
+    columnValue = document.createElement("div")
+    columnValue = document.createElement("row")
+    columnValue.classList.add("col")
+    columnValue.innerHTML = student.gender
+    row.appendChild(columnName)
+    row.appendChild(columnValue)
+    output.appendChild(row)
 }// document.getElementById('output').innerText = student;
-window.addEventListener("load",function(){
+/*window.addEventListener("load",function(){
     addStudentData(student)
-})
+    addStudentData(student2)
+})*/
+var students = [student,student2,{name:'llll',username:'sdasd',gender:'หญิง'}]
+function addStudentTotable(index,student){
+    const tableBody = document.getElementById('tableBody')
+    let row = document.createElement('tr')
+    let cell = document.createElement('th')
+    cell.setAttribute('score','row')
+    cell.innerHTML = index
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = student.name
+    row.appendChild(cell)
+    cell = document.createElement('td')
+   // cell.innerHTML = student.username
+    let img = document.createElement('img')
+    img.setAttribute('src',student.imageLink)
+    cell.appendChild(img)
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = student.gender
+    row.appendChild(cell)
+    tableBody.appendChild(row)
+}  
+function addStudentList(studentList){
+    let counter = 1
+    for(student of studentList){
+        addStudentTotable(counter++,student)
+    }
+} 
+/*window.addEventListener("load",function(){
+    addStudentList(students)
+})*/
+function onLoad(){
+
+    fetch('asset/students2.json').then(response => {
+        return response.json()
+    })
+        .then(data => {
+            let students = data
+            console.log(data)
+            addStudentList(data)
+        })
+}
